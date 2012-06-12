be1324706ba54b0ce6e1b95b5282594495afcb46
Mark Hayes <mark@zurb.com>
2012-06-12 15:33:42 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codefather::Application.config.secret_token = '20b76ceae0492e93072d75c5d5fd99e8819c5fe52f068562c5fc6c9e417be230de26ee076de3c4ae8c1f1668bef55c9f672718b90ad99b6d1a065f6fe4918d2e'
